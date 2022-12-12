73e3cde3bb7404c0f17aa41bfc927f907c9d9baa
Uuno <105356688+Unskutin@users.noreply.github.com>
2022-12-12 12:50:42 +0200

    Add files via upload

---
+++ henkilotunnustarkistus.php
@@ -0,0 +1,60 @@
+<?php
+require_once "database.php";
+$vaarin = "";
+$lista = array('+','-','A');
+$lista2 = array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','H','J','K','L','M','N','P','R','S','T','U','V','W','X','Y');
+$lista3 = array(31,28,31,30,31,30,31,31,30,31,30,31);
+if(!empty($_POST)){
+    $henkilotunnus = $_POST['henkilotunnus'];
+
+if ((strlen($henkilotunnus)!=11 && ($henkilotunnus != null))){ //katsoo onko henkilötunnus oikean pituinen
+    $vaarin = "henkilötunnus on väärän pituinen";
+}elseif(empty($henkilotunnus)){
+    $vaarin = "syötä henkilötunnus";
+}else{//tarkistaa onko viimeinen merkki oikea
+    $viimeinen = (substr($henkilotunnus,0,6) . substr($henkilotunnus,7,3))%31;
+if (substr($henkilotunnus,10,1)!=$lista2[$viimeinen]){
+        $vaarin = "viimeinen merkki on väärin";
+    
+}
+if ((substr($henkilotunnus,0,2)>$lista3[substr($henkilotunnus,2,2)]) || (substr($henkilotunnus,0,2)<1)){//katsoo onko ensimmäinen ja toinen numero oikeat
+    $vaarin = "ensimmäinen ja/tai toinen numero on väärin";
+}
+if ((substr($henkilotunnus,2,2)>12) || (substr($henkilotunnus,2,2)<1)){ //katsoo onko kolmas ja neljäs numero oikeat
+    $vaarin = "kolmas ja/tai neljäs numero on väärin";
+}
+if (in_array((substr($henkilotunnus,6,1)),$lista)==false){//katsoo laitatko vuosisadan tunnuksen
+    $vaarin ="Laita seitsemäs merkki";
+}
+if (substr($henkilotunnus,7,3) < 2){//tarkistaa kahdeksannen, yhdeksännen ja kymmenennen numeron
+    $vaarin = "kahdeksas ja/tai yhdeksäs ja/tai kymmenes numero on väärin";
+}
+}
+
+if($vaarin != ""){
+    echo $vaarin;
+}else{
+    echo "Henkilötunnus kelpaa";
+}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="henkilotunnustarkistus.php" method="POST">
+    <div>
+    henkilötunnus: <input id="henkilotunnus" name="henkilotunnus" type="text"><br>
+    </div>
+    <div>
+    <input type="submit">
+    </div>
+</form>
+</body>
+</html>
